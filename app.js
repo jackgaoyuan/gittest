7db440ad746f8c9699913b3880c1751d2a7a396e
gao yuan <jackgaoyuan@didiglobal.com>
2020-07-07 14:25:34 +0800

    master change name

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 /*eslint-disable */
-const name = "qa change name"
-<<<<<<< HEAD
-const year = 'master year'
-=======
+const name = "master change name"
 const year = 'qa change year'
->>>>>>> qa change year
 
 const qa = "qa"
